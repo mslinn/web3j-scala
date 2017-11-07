13e5722d6ab7a737d9728cb00ed642e7b48356cf
Mike Slinn <mslinn@micronauticsresearch.com>
2017-11-07 14:27:20 -0800

    -

--- src/main/scala/com/micronautics/web3j/package.scala
+++ src/main/scala/com/micronautics/web3j/package.scala
@@ -1,7 +1,8 @@
 package com.micronautics
 
-/** <img src='https://docs.web3j.io/_static/web3j.png' align='right' width='15%'>
+/** <img src='https://docs.web3j.io/_static/web3j.png' align='right'>
   * This project is an idiomatic Scala wrapper around [[https://www.web3j.io Web3J]] for Ethereum.
   * This project is built with Scala 2.12, and requires the Java 8 runtime; it is not yet compatible with Java 9.
-  * The [JSON-RPC documentation](https://github.com/ethereum/wiki/wiki/JSON-RPC#json-rpc-methods) was the source of the comments */
+  * The [JSON-RPC documentation](https://github.com/ethereum/wiki/wiki/JSON-RPC)
+  * was the source of the comments */
 package object web3j
