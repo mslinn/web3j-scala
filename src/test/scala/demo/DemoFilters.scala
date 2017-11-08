d23697f92f9b4e0ec2779aa5d62d6e0939737071
Mike Slinn <mslinn@micronauticsresearch.com>
2017-11-08 10:42:35 -0800

    Cleaning up

--- src/test/scala/demo/DemoFilters.scala
+++ src/test/scala/demo/DemoFilters.scala
@@ -2,27 +2,25 @@ package demo
 
 import rx.Subscription
 
-/**
-  * @author mslin_000 */
 class DemoFilters(demo: Demo) {
   import demo._
 
   //  web3j functional-reactive nature makes it really simple to setup observers that notify subscribers of events taking place on the blockchain.
   //  To receive all new blocks as they are added to the blockchain:
-    // todo implement Web3jRx wrapper
-    val subscription1: Subscription = web3j.blockObservable(false).subscribe { block =>
-      ???
-    }
+  // todo implement Web3jRx wrapper
+  val subscription1: Subscription = web3j.blockObservable(false).subscribe { block =>
+    ???
+  }
 
-  //  To receive all new transactions as they are added to the blockchain:
-    val subscription2: Subscription = web3j.transactionObservable.subscribe { tx =>
-      ???
-    }
+//  To receive all new transactions as they are added to the blockchain:
+  val subscription2: Subscription = web3j.transactionObservable.subscribe { tx =>
+    ???
+  }
 
-    //  To receive all pending transactions as they are submitted to the network (i.e. before they have been grouped into a block together):
-    val subscription3: Subscription = web3j.pendingTransactionObservable.subscribe { tx =>
-      ???
-    }
+  //  To receive all pending transactions as they are submitted to the network (i.e. before they have been grouped into a block together):
+  val subscription3: Subscription = web3j.pendingTransactionObservable.subscribe { tx =>
+    ???
+  }
 
     //  Or, if you’d rather replay all blocks to the most current, and be notified of new subsequent blocks being created:
   //  val subscription = web3j.catchUpToLatestAndSubscribeToNewBlocksObservable(startBlockNumber, fullTxObjects).subscribe { block =>
@@ -37,11 +35,11 @@ class DemoFilters(demo: Demo) {
   //    ???
   //  }
 
-    //  Subscriptions should always be cancelled when no longer required:
-    subscription1.unsubscribe()
-    subscription2.unsubscribe()
-    subscription3.unsubscribe()
+  //  Subscriptions should always be cancelled when no longer required:
+  subscription1.unsubscribe()
+  subscription2.unsubscribe()
+  subscription3.unsubscribe()
 
-    //  Note: filters are not supported on Infura.
-    //  For further information refer to Filters and Events and the Web3jRx interface.
+  //  Note: filters are not supported on Infura.
+  //  For further information refer to Filters and Events and the Web3jRx interface.
 }
