f1c3015286f68013fdc6b6940cfc2bdc67f67e8b
Mike Slinn <mslinn@micronauticsresearch.com>
2017-11-07 20:40:09 -0800

    -

--- src/main/scala/com/micronautics/web3j/package.scala
+++ src/main/scala/com/micronautics/web3j/package.scala
@@ -13,13 +13,16 @@ package com.micronautics
   * The [[https://jar-download.com/javaDoc/org.web3j/core/3.0.2/index.html?org/web3j/protocol/core/DefaultBlockParameter.html Java implementation]]
   * performs the same function as the JSON-RPC analog.
   *
-  * [[https://github.com/ethereum/wiki/wiki/Whisper Whisper]] is a communication protocol of the Ethereum P2P protocol suite that allows
+  * [[https://github.com/ethereum/wiki/wiki/Whisper Whisper]]
+  * is a communication protocol of the Ethereum P2P protocol suite that allows
   * for messaging between users' DApps (Ethereum Distributed Applications).
   * Whisper uses the same network that the blockchain runs on.
   * The protocol is separate from the blockchain, so smart contracts do not have access to whispers.
   *
   * [[https://jar-download.com/javaDoc/org.web3j/core/3.0.2/index.html?org/web3j/protocol/core/methods/response/EthBlock.html EthBlock]]
-  * and its internal objects such as [[https://jar-download.com/javaDoc/org.web3j/core/3.0.2/index.html?org/web3j/protocol/core/methods/response/EthBlock.Block.html Block]]
-  * are returned by the following methods: eth_getBlockByHash, eth_getBlockByNumber, eth_getUncleByBlockHashAndIndex, eth_getUncleByBlockNumberAndIndex.
+  * and its internal objects such as
+  * [[https://jar-download.com/javaDoc/org.web3j/core/3.0.2/index.html?org/web3j/protocol/core/methods/response/EthBlock.Block.html Block]]
+  * are returned by the following methods: {{{eth_getBlockByHash}}}, {{{eth_getBlockByNumber}}},
+  * {{{eth_getUncleByBlockHashAndIndex}}}, {{{eth_getUncleByBlockNumberAndIndex}}}.
   */
 package object web3j
