caf8773108b37096f2c9780d5761ebc39c47ed23
Mike Slinn <mslinn@micronauticsresearch.com>
2017-11-11 10:55:44 -0800

    -

--- src/test/scala/EtherTest.scala
+++ src/test/scala/EtherTest.scala
@@ -42,75 +42,75 @@ class EtherTest extends WordSpec {
     }
 
     "convert properly" in {
-      Ether.fromWei(BigInt(1))    shouldBe wei
-      Ether.fromWei(BigInt(1))    shouldBe wei
-      Ether.fromWei(1)            shouldBe wei
-      Ether.fromWei(1.0)          shouldBe wei
-      Ether.fromWei(1.0)          shouldBe wei
-      Ether.fromWei(1)            shouldBe wei
+      Ether.fromWei(BigInt(1))        shouldBe wei
+      Ether.fromWei(BigDecimal(1))    shouldBe wei
+      Ether.fromWei(1)                shouldBe wei
+      Ether.fromWei(1.0)              shouldBe wei
+      Ether.fromWei(1.0)              shouldBe wei
+      Ether.fromWei(1)                shouldBe wei
 
-      Ether.fromKWei(BigInt(1))   shouldBe kWei
-      Ether.fromKWei(BigInt(1))   shouldBe kWei
-      Ether.fromKWei(1)           shouldBe kWei
-      Ether.fromKWei(1.0)         shouldBe kWei
-      Ether.fromKWei(1.0)         shouldBe kWei
-      Ether.fromKWei(1)           shouldBe kWei
+      Ether.fromKWei(BigInt(1))       shouldBe kWei
+      Ether.fromKWei(BigDecimal(1))   shouldBe kWei
+      Ether.fromKWei(1)               shouldBe kWei
+      Ether.fromKWei(1.0)             shouldBe kWei
+      Ether.fromKWei(1.0)             shouldBe kWei
+      Ether.fromKWei(1)               shouldBe kWei
 
-      Ether.fromMWei(BigInt(1))   shouldBe mWei
-      Ether.fromMWei(BigInt(1))   shouldBe mWei
-      Ether.fromMWei(1)           shouldBe mWei
-      Ether.fromMWei(1.0)         shouldBe mWei
-      Ether.fromMWei(1.0)         shouldBe mWei
-      Ether.fromMWei(1)           shouldBe mWei
+      Ether.fromMWei(BigInt(1))       shouldBe mWei
+      Ether.fromMWei(BigDecimal(1))   shouldBe mWei
+      Ether.fromMWei(1)               shouldBe mWei
+      Ether.fromMWei(1.0)             shouldBe mWei
+      Ether.fromMWei(1.0)             shouldBe mWei
+      Ether.fromMWei(1)               shouldBe mWei
 
-      Ether.fromGWei(BigInt(1))   shouldBe gWei
-      Ether.fromGWei(BigInt(1))   shouldBe gWei
-      Ether.fromGWei(1)           shouldBe gWei
-      Ether.fromGWei(1.0)         shouldBe gWei
-      Ether.fromGWei(1.0)         shouldBe gWei
-      Ether.fromGWei(1)           shouldBe gWei
+      Ether.fromGWei(BigInt(1))       shouldBe gWei
+      Ether.fromGWei(BigDecimal(1))   shouldBe gWei
+      Ether.fromGWei(1)               shouldBe gWei
+      Ether.fromGWei(1.0)             shouldBe gWei
+      Ether.fromGWei(1.0)             shouldBe gWei
+      Ether.fromGWei(1)               shouldBe gWei
 
-      Ether.fromSzabo(BigInt(1))  shouldBe szabo
-      Ether.fromSzabo(BigInt(1))  shouldBe szabo
-      Ether.fromSzabo(1)          shouldBe szabo
-      Ether.fromSzabo(1.0)        shouldBe szabo
-      Ether.fromSzabo(1.0)        shouldBe szabo
-      Ether.fromSzabo(1)          shouldBe szabo
+      Ether.fromSzabo(BigInt(1))      shouldBe szabo
+      Ether.fromSzabo(BigDecimal(1))  shouldBe szabo
+      Ether.fromSzabo(1)              shouldBe szabo
+      Ether.fromSzabo(1.0)            shouldBe szabo
+      Ether.fromSzabo(1.0)            shouldBe szabo
+      Ether.fromSzabo(1)              shouldBe szabo
 
-      Ether.fromFinney(BigInt(1)) shouldBe finney
-      Ether.fromFinney(BigInt(1)) shouldBe finney
-      Ether.fromFinney(1)         shouldBe finney
-      Ether.fromFinney(1.0)       shouldBe finney
-      Ether.fromFinney(1.0)       shouldBe finney
-      Ether.fromFinney(1)         shouldBe finney
+      Ether.fromFinney(BigInt(1))     shouldBe finney
+      Ether.fromFinney(BigDecimal(1)) shouldBe finney
+      Ether.fromFinney(1)             shouldBe finney
+      Ether.fromFinney(1.0)           shouldBe finney
+      Ether.fromFinney(1.0)           shouldBe finney
+      Ether.fromFinney(1)             shouldBe finney
 
-      Ether.fromEther(BigInt(1))  shouldBe ether
-      Ether.fromEther(BigInt(1))  shouldBe ether
-      Ether.fromEther(1)          shouldBe ether
-      Ether.fromEther(1.0)        shouldBe ether
-      Ether.fromEther(1.0)        shouldBe ether
-      Ether.fromEther(1)          shouldBe ether
+      Ether.fromEther(BigInt(1))      shouldBe ether
+      Ether.fromEther(BigDecimal(1))  shouldBe ether
+      Ether.fromEther(1)              shouldBe ether
+      Ether.fromEther(1.0)            shouldBe ether
+      Ether.fromEther(1.0)            shouldBe ether
+      Ether.fromEther(1)              shouldBe ether
 
-      Ether.fromKEther(BigInt(1)) shouldBe kEther
-      Ether.fromKEther(BigInt(1)) shouldBe kEther
-      Ether.fromKEther(1)         shouldBe kEther
-      Ether.fromKEther(1.0)       shouldBe kEther
-      Ether.fromKEther(1.0)       shouldBe kEther
-      Ether.fromKEther(1)         shouldBe kEther
+      Ether.fromKEther(BigInt(1))     shouldBe kEther
+      Ether.fromKEther(BigDecimal(1)) shouldBe kEther
+      Ether.fromKEther(1)             shouldBe kEther
+      Ether.fromKEther(1.0)           shouldBe kEther
+      Ether.fromKEther(1.0)           shouldBe kEther
+      Ether.fromKEther(1)             shouldBe kEther
 
-      Ether.fromMEther(BigInt(1)) shouldBe mEther
-      Ether.fromMEther(BigInt(1)) shouldBe mEther
-      Ether.fromMEther(1)         shouldBe mEther
-      Ether.fromMEther(1.0)       shouldBe mEther
-      Ether.fromMEther(1.0)       shouldBe mEther
-      Ether.fromMEther(1)         shouldBe mEther
+      Ether.fromMEther(BigInt(1))     shouldBe mEther
+      Ether.fromMEther(BigDecimal(1)) shouldBe mEther
+      Ether.fromMEther(1)             shouldBe mEther
+      Ether.fromMEther(1.0)           shouldBe mEther
+      Ether.fromMEther(1.0)           shouldBe mEther
+      Ether.fromMEther(1)             shouldBe mEther
 
-      Ether.fromGEther(BigInt(1)) shouldBe gEther
-      Ether.fromGEther(BigInt(1)) shouldBe gEther
-      Ether.fromGEther(1)         shouldBe gEther
-      Ether.fromGEther(1.0)       shouldBe gEther
-      Ether.fromGEther(1.0)       shouldBe gEther
-      Ether.fromGEther(1)         shouldBe gEther
+      Ether.fromGEther(BigInt(1))     shouldBe gEther
+      Ether.fromGEther(BigDecimal(1)) shouldBe gEther
+      Ether.fromGEther(1)             shouldBe gEther
+      Ether.fromGEther(1.0)           shouldBe gEther
+      Ether.fromGEther(1.0)           shouldBe gEther
+      Ether.fromGEther(1)             shouldBe gEther
     }
   }
 }
