04cc6a39f004d5a720ae8a343636a4591f44e32a
Mike Slinn <mslinn@micronauticsresearch.com>
2017-11-11 16:41:14 -0800

    -

--- src/main/scala/com/micronautics/web3j/ValueClasses.scala
+++ src/main/scala/com/micronautics/web3j/ValueClasses.scala
@@ -4,7 +4,7 @@ import java.math.BigInteger
 import org.web3j.crypto.WalletUtils
 
 object Address {
-  /** This implicit conversion allows for a convenient way of constructing an instance from a [[java.lang.String]].
+  /** This implicit conversion allows for a convenient way of constructing an `Address` instance from a [[java.lang.String]].
     * {{{val address: Address = "0xdeadbeef"}}} */
   implicit def stringToAddress(value: String): Address = Address(value)
 }
@@ -15,7 +15,7 @@ case class Address(value: String) extends AnyVal {
 
 
 object Compiler {
-  /** This implicit conversion allows for a convenient way of constructing an instance from a [[java.lang.String]].
+  /** This implicit conversion allows for a convenient way of constructing a `Compiler` instance from a [[java.lang.String]].
     * {{{val compiler: Compiler = "solc"}}} */
   implicit def stringToCompiler(value: String): Compiler = Compiler(value)
 }
@@ -26,7 +26,7 @@ case class Compiler(value: String) extends AnyVal {
 
 
 object Digest {
-  /** This implicit conversion allows for a convenient way of constructing an instance from a [[java.lang.String]].
+  /** This implicit conversion allows for a convenient way of constructing a `Digest` instance from a [[java.lang.String]].
     * {{{val digest: Digest = "0xdeadbeef"}}} */
   implicit def stringToDigest(value: String): Digest = Digest(value)
 }
@@ -37,7 +37,7 @@ case class Digest(value: String) extends AnyVal {
 
 
 object EtHash {
-  /** This implicit conversion allows for a convenient way of constructing an instance from a [[java.lang.String]].
+  /** This implicit conversion allows for a convenient way of constructing an `EtHash` instance from a [[java.lang.String]].
     * {{{val etHash: EtHash = "0xdeadbeef"}}} */
   implicit def stringToEtHash(value: String): EtHash = EtHash(value)
 }
@@ -49,7 +49,7 @@ case class EtHash(value: String) extends AnyVal {
 
 
 object BlockHash {
-  /** This implicit conversion allows for a convenient way of constructing an instance from a [[java.lang.String]].
+  /** This implicit conversion allows for a convenient way of constructing a `BlockHash` instance from a [[java.lang.String]].
     * {{{val blockHash: BlockHash = "0xdeadbeef"}}} */
   implicit def stringToBlockHash(value: String): BlockHash = BlockHash(value)
 }
@@ -60,7 +60,7 @@ case class BlockHash(value: String) extends AnyVal {
 
 
 object FilterId {
-  /** This implicit conversion allows for a convenient way of constructing an instance from a [[java.math.BigInteger]].
+  /** This implicit conversion allows for a convenient way of constructing a `FilterId` instance from a [[java.math.BigInteger]].
     * {{{val filterId: FilterId = "0xdeadbeef"}}} */
   implicit def stringToFilterId(value: BigInteger): FilterId = FilterId(value)
 }
@@ -71,7 +71,7 @@ case class FilterId(value: BigInteger) extends AnyVal {
 
 
 object LLLCompiled {
-  /** This implicit conversion allows for a convenient way of constructing an instance from a [[java.lang.String]].
+  /** This implicit conversion allows for a convenient way of constructing a `LLLCompiled` instance from a [[java.lang.String]].
     * {{{val lLLCompiled: LLLCompiled = "{hello, world}"}}} */
   implicit def stringToLLLCompiled(value: String): LLLCompiled = LLLCompiled(value)
 }
@@ -82,7 +82,7 @@ case class LLLCompiled(value: String) extends AnyVal {
 
 
 object LLLSource {
-  /** This implicit conversion allows for a convenient way of constructing an instance from a [[java.lang.String]].
+  /** This implicit conversion allows for a convenient way of constructing a `LLLSource` instance from a [[java.lang.String]].
     * {{{val lLLSource: LLLSource = "{hello, world}"}}} */
   implicit def stringToLLLSource(value: String): LLLSource = LLLSource(value)
 }
@@ -93,7 +93,7 @@ case class LLLSource(value: String) extends AnyVal {
 
 
 object Nonce {
-  /** This implicit conversion allows for a convenient way of constructing an instance from a [[scala.BigInt]].
+  /** This implicit conversion allows for a convenient way of constructing a `Nonce` instance from a [[scala.BigInt]].
     * {{{val nonce: Nonce = "0x123456789"}}} */
   implicit def stringToNonce(value: BigInt): Nonce = Nonce(value)
 }
@@ -110,7 +110,7 @@ case class Nonce(value: BigInt) extends AnyVal {
 
 
 object PrivateKey {
-  /** This implicit conversion allows for a convenient way of constructing an instance from a [[java.lang.String]].
+  /** This implicit conversion allows for a convenient way of constructing a `PrivateKey` instance from a [[java.lang.String]].
     * {{{val privateKey: PrivateKey = "blah blah blah"}}} */
   implicit def stringToPrivateKey(value: String): PrivateKey = PrivateKey(value)
 }
@@ -123,7 +123,7 @@ case class PrivateKey(value: String) extends AnyVal {
 
 
 object PublicKey {
-  /** This implicit conversion allows for a convenient way of constructing an instance from a [[java.lang.String]].
+  /** This implicit conversion allows for a convenient way of constructing a `PublicKey` instance from a [[java.lang.String]].
     * {{{val publicKey: PublicKey = "blah blah blah"}}} */
   implicit def stringToPublicKey(value: String): PublicKey = PublicKey(value)
 }
@@ -134,7 +134,7 @@ case class PublicKey(value: String) extends AnyVal {
 
 
 object SerpentCompiled {
-  /** This implicit conversion allows for a convenient way of constructing an instance from a [[java.lang.String]].
+  /** This implicit conversion allows for a convenient way of constructing a `SerpentCompiled` instance from a [[java.lang.String]].
     * {{{val serpentCompiled: SerpentCompiled = "{hello, world}"}}} */
   implicit def stringToSerpentCompiled(value: String): SerpentCompiled = SerpentCompiled(value)
 }
@@ -145,7 +145,7 @@ case class SerpentCompiled(value: String) extends AnyVal {
 
 
 object SerpentSource {
-  /** This implicit conversion allows for a convenient way of constructing an instance from a [[java.lang.String]].
+  /** This implicit conversion allows for a convenient way of constructing a `SerpentSource` instance from a [[java.lang.String]].
     * {{{val serpentSource: SerpentSource = "{hello, world}"}}} */
   implicit def stringToSerpentSource(value: String): SerpentSource = SerpentSource(value)
 }
@@ -156,7 +156,7 @@ case class SerpentSource(value: String) extends AnyVal {
 
 
 object Keccak256Hash {
-  /** This implicit conversion allows for a convenient way of constructing an instance from a [[java.lang.String]].
+  /** This implicit conversion allows for a convenient way of constructing a `Keccak256Hash` instance from a [[java.lang.String]].
     * {{{val keccak256Hash: Keccak256Hash = "0xdeadbeef"}}} */
   implicit def stringToKeccak256Hash(value: String): Keccak256Hash = Keccak256Hash(value)
 }
@@ -171,7 +171,7 @@ case class Keccak256Hash(value: String) extends AnyVal {
 
 
 object Password {
-  /** This implicit conversion allows for a convenient way of constructing an instance from a [[java.lang.String]].
+  /** This implicit conversion allows for a convenient way of constructing a `Password` instance from a [[java.lang.String]].
     * {{{val password: Password = "secret"}}} */
   implicit def stringToPassword(value: String): Password = Password(value)
 }
@@ -182,7 +182,7 @@ case class Password(value: String) extends AnyVal {
 
 
 object Signature {
-  /** This implicit conversion allows for a convenient way of constructing an instance from a [[java.lang.String]].
+  /** This implicit conversion allows for a convenient way of constructing a `Signature` instance from a [[java.lang.String]].
     * {{{val signature: Signature = "0xdeadbeef"}}} */
   implicit def stringToSignature(value: String): Signature = Signature(value)
 }
@@ -193,7 +193,7 @@ case class Signature(value: String) extends AnyVal {
 
 
 object SignedData {
-  /** This implicit conversion allows for a convenient way of constructing an instance from a [[java.lang.String]].
+  /** This implicit conversion allows for a convenient way of constructing a `SignedData` instance from a [[java.lang.String]].
     * {{{val signedData: SignedData = "0xdeadbeef"}}} */
   implicit def stringToSignedData(value: String): SignedData = SignedData(value)
 }
@@ -204,7 +204,7 @@ case class SignedData(value: String) extends AnyVal {
 
 
 object SoliditySource {
-  /** This implicit conversion allows for a convenient way of constructing an instance from a [[java.lang.String]].
+  /** This implicit conversion allows for a convenient way of constructing a `SoliditySource` instance from a [[java.lang.String]].
     * {{{val soliditySource: SoliditySource = "{hello world}"}}} */
   implicit def stringToSoliditySource(value: String): SoliditySource = SoliditySource(value)
 }
@@ -216,7 +216,7 @@ case class SoliditySource(value: String) extends AnyVal {
 
 
 object TransactionHash {
-  /** This implicit conversion allows for a convenient way of constructing an instance from a [[java.lang.String]].
+  /** This implicit conversion allows for a convenient way of constructing a `TransactionHash` instance from a [[java.lang.String]].
     * {{{val transactionHash: TransactionHash = "0xdeadbeef"}}} */
   implicit def stringToTransactionHash(value: String): TransactionHash = TransactionHash(value)
 }
